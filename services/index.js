46e2c96c6b84a6b20a33cd74263f203fd93e9334
mengchieh123 <110330444+mengchieh123@users.noreply.github.com>
2025-11-07 02:11:24 +0000

    Add services

--- services/index.js
+++ services/index.js
@@ -1,24 +1,11 @@
-// services/index.js
-// 服務層導出檔案
-
-const bookingService = require('./bookingService');
-const pricingService = require('./pricingService');
-const memberService = require('./memberService');
-const roomStatusService = require('./roomStatusService');
-const promotionService = require('./promotionService');
-const paymentService = require('./paymentService');
-const invoiceService = require('./invoiceService');
-const complianceService = require('./complianceService');
-const localizationService = require('./localizationService');
-
 module.exports = {
-  bookingService,
-  pricingService,
-  memberService,
-  roomStatusService,
-  promotionService,
-  paymentService,
-  invoiceService,
-  complianceService,
-  localizationService
+  bookingService: require('./bookingService'),
+  complianceService: require('./complianceService'),
+  invoiceService: require('./invoiceService'),
+  localizationService: require('./localizationService'),
+  memberService: require('./memberService'),
+  paymentService: require('./paymentService'),
+  pricingService: require('./pricingService'),
+  promotionService: require('./promotionService'),
+  roomStatusService: require('./roomStatusService'),
 };
